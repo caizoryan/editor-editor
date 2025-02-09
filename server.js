fcf2c6f5625a6fa4e7ba73a77a91c2087c90064f
Aaryan Pashine <pashineaaryan@gmail.com>
2025-02-08 20:41:09 -0500

    added saving and positioning

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import express from "express";
 import path from "path";
 import process from "process";
+import bodyParser from "body-parser";
 
 // **********************************
 // ----------------------------------
@@ -38,7 +39,17 @@ const CONFIG = {
 // OVERWRITE FILE
 // ----------------------------------
 // **********************************
-function overwrite_path(req, res) { }
+function overwrite_path(req, res) {
+	const file_path = req.path.replace("/fs", "");
+	const full_path = path.join(process.cwd(), CONFIG.DIR, file_path);
+	const body = req.body;
+	console.log("overwriting", file_path);
+	console.log("body", body);
+
+	if (!body) return res.status(400).send("No body provided");
+
+	has_extension(file_path) ? write_file(full_path, body) : write_dir(full_path);
+}
 
 // **********************************
 // ----------------------------------
@@ -49,19 +60,27 @@ function write_path(req, res) {
 	// const options = { root: path.join(process.cwd()) };
 
 	const file_path = req.path.replace("/fs", "");
+	const full_path = path.join(process.cwd(), CONFIG.DIR, file_path);
 	const body = req.body;
 
 	if (!body) return res.status(400).send("No body provided");
+
 	// Will check if file exists and create it if not
 	// If file exists, will return error
-
-	if (fs.existsSync(CONFIG.DIR + file_path)) {
+	if (fs.existsSync(CONFIG.DIR + full_path)) {
 		return res.status(400).send("File already exists");
 	}
+
+	has_extension(file_path) ? write_file(full_path, body) : write_dir(full_path);
 }
 
-function write_file(path, body) { }
-function write_dir(path) { }
+function write_file(path, body) {
+	fs.writeFileSync(path, JSON.stringify(body, null, 2));
+}
+
+function write_dir(path) {
+	fs.mkdirSync(path, { recursive: true });
+}
 
 // **********************************
 // ----------------------------------
@@ -85,7 +104,6 @@ function write_dir(path) { }
  */
 const get_path_base = (root) => (req, res) => {
 	const options = { root: path.join(process.cwd()) };
-	console.log("req path", req.path);
 	const file_path = req.path.replace(root, "");
 	console.log("file_path", file_path);
 
@@ -98,8 +116,24 @@ const get_path_base = (root) => (req, res) => {
 	else if (file_res.type === "dir") res.json(file_res);
 };
 
+const get_exists_base = (root) => (req, res) => {
+	const file_path = req.path.replace("/exists/", "");
+	console.log("exists called ->", file_path);
+
+	const file_res = has_extension(file_path)
+		? get_file(file_path, root)
+		: get_directory(file_path);
+
+	const result = {
+		exists: file_res ? true : false,
+	};
+
+	res.json(result);
+};
+
 const get_path = get_path_base(CONFIG.DIR);
 const get_library = get_path_base(CONFIG.LIB);
+const get_exists = get_exists_base(CONFIG.DIR);
 //
 //
 /** ---------------------------------
@@ -136,9 +170,12 @@ function has_extension(str) {
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
 	res.sendFile("index.html", { root: path.join(process.cwd()) });
 });
+app.get("/exists/*", get_exists);
 app.get("/fs/*", get_path);
 app.get("/lib/*", get_library);
 
